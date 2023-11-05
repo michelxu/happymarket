c8903fe6f4efbe91b4214a9d7decaa24aa20b6b6
michelxu <jeanmbust@hotmail.com>
2023-11-05 16:23:50 -0700

    start

---
+++ src/test/java/com/happy/market/HappyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
